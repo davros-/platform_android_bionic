bf2259ff0a6fc9cf7375a11608490ae81b1e8cce
Stevespear426 <stephen.k.spear@gmail.com>
2013-01-14 02:06:43 +0100

    add back bzero
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- libc/Android.mk
+++ libc/Android.mk
@@ -452,6 +452,7 @@ ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
 libc_common_src_files += \
 	arch-arm/bionic/armv7/memchr.S \
 	arch-arm/bionic/armv7/memset.S \
+    arch-arm/bionic/armv7/bzero.S \
 	arch-arm/bionic/armv7/strchr.S \
 	arch-arm/bionic/strcpy.S \
 	arch-arm/bionic/strlen-armv7.S
